24bdb7288473d77ab89bca58692bb4b9987d55a7
Gabi <gabriella.pshibata@gmail.com>
2024-02-28 16:13:52 -0300

    Alteração no código

--- POO.py
+++ POO.py
@@ -7,15 +7,22 @@ class Restaurante:
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
-        self.ativo = False
+        self._ativo = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
         return f'{self.nome} | {self.categoria}'
     
     def listar_restaurantes():
+        print(f'{'Nome do restaurante'.ljust(25)} < {'Categoria do restaurante'.ljust(25)} < {'Status do restaurante'}')
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} > {restaurante.categoria} > {restaurante.ativo}')
+            print(f'{restaurante.nome.ljust(25)} > {restaurante.categoria.ljust(25)} > {restaurante.ativo}')
+
+    #PROPERTY = usado para modificar o atributo. Para fazer operações matemáticas, pegar vários valores e agrupar em um só, etc
+
+    @property
+    def ativo(self):
+        return 'verdadeiro' if self._ativo else 'falso'
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
